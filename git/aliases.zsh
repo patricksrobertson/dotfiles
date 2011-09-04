ee43194c3e2a0a361436c3fb7618108b4324af3d
Will Mernagh <wmernagh@gmail.com>
2011-09-04 01:23:16 -0400

    add gmerged alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -27,5 +27,6 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gmerged='echo "Merged";git branch -a --merged;echo "Not Merged";git branch -a --no-merge'
 alias sourcehitch='source ~/.hitch_export_authors'
 alias unhitch='`which hitch` -u;echo "export GIT_AUTHOR_NAME=\"Will Mernagh\" GIT_AUTHOR_EMAIL=\"wmernagh@gmail.com\"" > ~/.hitch_export_authors;source ~/.hitch_export_authors'
