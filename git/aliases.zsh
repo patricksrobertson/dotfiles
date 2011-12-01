3e0020c3f62b30de33273a4b4a9bcd21ca227828
Will Mernagh <wmernagh@gmail.com>
2011-11-30 22:11:35 -0500

    git improvments

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,21 +1,4 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-#
-# I use JRuby substantially, and we want to make sure hub is run using MRI
-# regardless of which Ruby you're using or else the `git status` in your prompt
-# will take seven thousand seconds to run `ls`.
-#
-# I'm hardcoding it to an installed rvm (using rvm's `rvm 1.8.7,ruby /hub/path`
-# syntax is way too slow). It should work fine for those without rvm, though.
-if [[ -s $HOME/.rvm/scripts/rvm ]]
-then
-  if $(which hub &> /dev/null) && [[ -s $HOME/.rvm/rubies/ruby-1.8.7-p334 ]]
-  then
-    alias git='$HOME/.rvm/rubies/ruby-1.8.7-p334/bin/ruby `which hub`'
-  else
-  fi
-fi
-
+# alias git=hub
 # The rest of my fun git aliases
 alias gl='git pull --rebase --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -24,9 +7,11 @@ alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
-alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gb='git branch -a'
+alias gwm='git branch -a|grep wm'
+alias gm='git branch -a --merged'
 alias gmerged='echo "Merged";git branch -a --merged;echo "Not Merged";git branch -a --no-merge'
 alias sourcehitch='source ~/.hitch_export_authors'
 alias unhitch='`which hitch` -u;echo "export GIT_AUTHOR_NAME=\"Will Mernagh\" GIT_AUTHOR_EMAIL=\"wmernagh@gmail.com\"" > ~/.hitch_export_authors;source ~/.hitch_export_authors'
